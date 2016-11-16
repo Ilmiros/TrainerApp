d3c4f5125bc1a3a4d74b56478df9e4489dd9b28e
LAPTOP-TO31IR5G\ramil <ramilxp2009@mail.ru>
2016-11-16 22:36:57 +0300

    бд

--- TrainerApp/TrainerApp/HelpDB/EFDBContext.cs
+++ TrainerApp/TrainerApp/HelpDB/EFDBContext.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using TrainerApp.Entities;
 
 namespace TrainerApp.HelpDB
 {
-    public class EFDBContext
+    public class EFDBContext:DbContext
     {
+        private DbSet<User> Users { get; set; }
     }
 }
