606d68a3e699eeeb4d5efe97b43e2dafb73a3069
LAPTOP-TO31IR5G\ramil <ramilxp2009@mail.ru>
2016-11-16 22:28:39 +0300

    Добавил подключение к базе

--- TrainerApp/TrainerApp/Entities/User.cs
+++ TrainerApp/TrainerApp/Entities/User.cs
@@ -7,7 +7,9 @@ namespace TrainerApp.Entities
 {
     public class User
     {
+        public int Id { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
+        public string Email { get; set; }
     }
 }
